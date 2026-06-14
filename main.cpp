ef51b377862db579da1614c4c737f465dea404e9
Your Name <jasonnguyen7506@gmail.com>
2026-06-14 00:45:20 -0500

    task 1 parse commands

--- main.cpp
+++ main.cpp
@@ -6,4 +6,22 @@ int main(int argc, char* argv[]) {
     for (int i = 0; i < argc; i++) {
         std::cout << "argv[" << i << "] " << argv[i] << std::endl;
     }
+
+    std::string command = argv[1];
+
+    if (command == "add") {
+        std::cout << "add command recognized" << std::endl;
+    } else if (command == "list") {
+        std::cout << "list command recognized" << std::endl;
+    } else if (command == "update") {
+        std::cout << "update command recognized" << std::endl;
+    } else if (command == "delete") {
+        std::cout << "delete command recognized" << std::endl;
+    } else if (command == "mark-done") {
+        std::cout << "mark-done commmand recognized" << std::endl;
+    } else if (command == "mark-in-progress") {
+        std::cout << "mark-in-progress command recognized" << std::endl;
+    }
+
+
 }
