b3bcc180e33784d2dfe42cae31d1c50062c5f511
Your Name <jasonnguyen7506@gmail.com>
2026-06-14 17:31:16 -0500

    display json formatted for new task

--- main.cpp
+++ main.cpp
@@ -17,44 +17,63 @@ bool taskToJson(const Task& param) {
     std::string thirdLine = param.status;
     std::string fourthLine = param.createdAt;
     std::string fifthLine = param.updatedAt;
+    std::string tab = "    ";
 
-
-    outFile << firstLine << std::endl;
-    outFile << secondLine << std::endl;
-    outFile << thirdLine << std::endl;
-    outFile << fourthLine << std::endl;
-    outFile << fifthLine << std::endl;
+    outFile << "{" << std::endl;
+    outFile << tab << "\"id\": " << firstLine << "," << std::endl;
+    outFile << tab << "\"description\": " << "\"" << secondLine << "\"" << "," << std::endl;
+    outFile << tab << "\"status\": " << "\"" << thirdLine << "\"" << "," << std::endl;
+    outFile << tab << "\"createdAt\": " << "\"" << fourthLine << "\"" << "," << std::endl;
+    outFile << tab << "\"updatedAt\": " << "\"" << fifthLine << "\"" <<std::endl;
+    outFile << "}" << std::endl;
 
     return true;
 }
 
 int main(int argc, char* argv[]) {
-    if (argc < 3) {
+    if (argc < 2) {
         throw std::runtime_error("Must have atleast 3 args");
     }
 
     std::cout << "Received " << argc << " arguments:\n";
 
     std::string command = argv[1];
-    std::string identifer = argv[2];
-    std::string description = argv[3];
+    std::string task = argv[2];
 
     if (command == "add") {
-        Task newFile = createTask(description);
+        Task newFile = createTask(task);
         taskToJson(newFile);
+        std::cout << "Task added successfully (ID: " << newFile.id << ")" << std::endl;
 
     } else if (command == "list") {
-        std::cout << "list command recognized" << std::endl;
-    } else if (command == "update") {
-        std::cout << "update command recognized" << std::endl;
-    } else if (command == "delete") {
-        std::cout << "delete command recognized" << std::endl;
-    } else if (command == "mark-done") {
-        std::cout << "mark-done commmand recognized" << std::endl;
-    } else if (command == "mark-in-progress") {
-        std::cout << "mark-in-progress command recognized" << std::endl;
-    }
 
-    
-    writeToFile("tasks.json");
+
+        std::cout << "list command recognized" << std::endl;
+
+
+    } else if (command == "update") {
+
+
+        std::cout << "update command recognized" << std::endl;
+
+
+    } else if (command == "delete") {
+
+
+        std::cout << "delete command recognized" << std::endl;
+
+
+    } else if (command == "mark-done") {
+
+
+        std::cout << "mark-done commmand recognized" << std::endl;
+
+
+    } else if (command == "mark-in-progress") {
+
+
+        std::cout << "mark-in-progress command recognized" << std::endl;
+
+
+    }
 }
